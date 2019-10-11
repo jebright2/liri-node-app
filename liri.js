6068a903ec6cc45507331fe5b8ecece41e04bc60
Da Vinci <jeb2je@gmail.com>
2019-10-11 19:59:56 -0400

    added user commands

--- liri.js
+++ liri.js
@@ -1,3 +1,37 @@
 require("dotenv").config();
+
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+var fs = require("fs");
+var moment = require('moment');
+var Spotify = require('node-spotify-api');
+
+//Set to handle user input
+var userCommand = process.argv[2]; 
+var userChoice = process.argv[3];
+
+//User Command Function
+function UserInput (userCommand, userChoice) {
+    switch(userCommand) {
+        case "concert-this":
+            concertInfo(userChoice);
+            break;
+        
+        case "spotify-this-song":
+            songInfo(userChoice);
+            break;
+
+        case "movie-this":
+            movieInfo(userChoice);
+            break;
+
+        case "do-what-it-says":
+            doStuff(userChoice);
+            break;
+
+        default:"Error! Please use one of the following commands to initiate your search: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says"
+    }
+
+}
+
+UserInput();
