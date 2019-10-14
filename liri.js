bd4155d3d84d7c53022e47da502e1d6ed651878f
Da Vinci <jeb2je@gmail.com>
2019-10-14 19:54:35 -0400

    building concert function

--- liri.js
+++ liri.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
 var fs = require("fs");
-var moment = require('moment');
+var axios = require ("axios");
 var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
 //Set to handle user input
 var userCommand = process.argv[2]; 
 var userChoice = process.argv[3];
+
+UserInput(userCommand, userChoice);
 
 //User Command Function
 function UserInput (userCommand, userChoice) {
@@ -29,9 +31,29 @@ function UserInput (userCommand, userChoice) {
             doStuff(userChoice);
             break;
 
-        default:"Error! Please use one of the following commands to initiate your search: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says"
+        default:
+            console.log("Error! Please use one of the following commands to initiate your search: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says");
     }
 
 }
 
-UserInput();
+//Function for Concert Information
+
+function concertInfo(userChoice) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + userChoice + "/events?app_id=codingbootcamp";
+    axios (queryURL, function (error, response, body) {
+
+    if(!error && response.statusCode === 200) {
+        var concerts = JSON.parse (body);
+        for (var i = 0; i < concerts.length; i++) {
+
+            console.log("*****EVENT INFORMATION******");
+            fs.appendFileSync("log.txt", "*****EVENT INFORMATION******\n");
+            console.log(i);
+            fs.appendFileSync("log.txt", i+"\n");
+            
+        }
+    }
+
+    })
+}
